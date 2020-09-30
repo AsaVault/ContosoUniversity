f246fa47011217f300bd6524f58fb2519309a9fe
AsaVault <57707680+AsaVault@users.noreply.github.com>
2020-09-30 05:57:08 +0100

    max length attribute added to student model class

--- Models/Student.cs
+++ Models/Student.cs
@@ -8,11 +8,11 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
         [StringLength(50)]
-        //public string LastName { get; set; }
-        //[StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-        //public string FirstMidName { get; set; }
-        //[DataType(DataType.Date)]
-        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
