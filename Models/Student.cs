4c25e5e6020430f423d06019241b84d0573887f6
AsaVault <57707680+AsaVault@users.noreply.github.com>
2020-09-30 06:00:07 +0100

    max length attribute updated to db

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace ContosoUniversity.Models
 {
     public class Student
