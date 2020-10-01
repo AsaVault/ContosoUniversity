92b5d0757c65a7dff9d571ea5e1788e8867ca228
AsaVault <57707680+AsaVault@users.noreply.github.com>
2020-10-01 11:01:34 +0100

    complex data model added to databse

--- Migrations/202010010950308_ComplexDataModel.Designer.cs
+++ Migrations/202010010950308_ComplexDataModel.Designer.cs
@@ -7,13 +7,13 @@ namespace ContosoUniversity.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class InitialCreate : IMigrationMetadata
+    public sealed partial class ComplexDataModel : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(ComplexDataModel));
         
         string IMigrationMetadata.Id
         {
-            get { return "202009230925552_InitialCreate"; }
+            get { return "202010010950308_ComplexDataModel"; }
         }
         
         string IMigrationMetadata.Source
