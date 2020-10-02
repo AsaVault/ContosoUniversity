f6d29ef08a9e6f782ee5fa86d6ed35b345a96c0d
AsaVault <57707680+AsaVault@users.noreply.github.com>
2020-10-02 20:12:49 +0100

    Added UpdateInstructorCourses method to instructor controller

--- Controllers/InstructorController.cs
+++ Controllers/InstructorController.cs
@@ -199,6 +199,37 @@ namespace ContosoUniversity.Controllers
             ViewBag.Courses = viewModel;
         }
 
+        // Added UpdateInstructorCourses
+        private void UpdateInstructorCourses(string[] selectedCourses, Instructor instructorToUpdate)
+        {
+            if (selectedCourses == null)
+            {
+                instructorToUpdate.Courses = new List<Course>();
+                return;
+            }
+
+            var selectedCoursesHS = new HashSet<string>(selectedCourses);
+            var instructorCourses = new HashSet<int>
+                (instructorToUpdate.Courses.Select(c => c.CourseID));
+            foreach (var course in db.Courses)
+            {
+                if (selectedCoursesHS.Contains(course.CourseID.ToString()))
+                {
+                    if (!instructorCourses.Contains(course.CourseID))
+                    {
+                        instructorToUpdate.Courses.Add(course);
+                    }
+                }
+                else
+                {
+                    if (instructorCourses.Contains(course.CourseID))
+                    {
+                        instructorToUpdate.Courses.Remove(course);
+                    }
+                }
+            }
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
